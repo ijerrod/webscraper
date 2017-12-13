272b27e0b3b2aa49d1fe74fd9297ef4363d538d2
unknown <jlwelsh@gmail.com>
2017-12-13 12:42:41 -0600

    added comments and write output to csv

--- links_scrape/link_scrape.py
+++ links_scrape/link_scrape.py
@@ -1,15 +1,33 @@
+# Import required modules
 from bs4 import BeautifulSoup
 import requests
+import csv
 
+# Prompt user to enter a URL
 url = input("Enter a Website URL to scrape its links: ")
 
+# Opens and reads URL
 r = requests.get(url)
-
 data = r.text
 
+# close open session
+r.close()
+
+# HTML Parser
 soup = BeautifulSoup(data, 'html.parser')
 
-linkList = ''
+# Set up CSV file
+filename = "links.csv"
+f = open(filename, "w")
+headers = "links\n"
+f.write(headers)
 
-for link in soup.findAll('a'):
-    print(link.get('href'))
+# Print each link found
+for link in soup.find_all('a'):
+    pLink = link.get('href')
+
+    print(pLink)
+
+    f.write(pLink + "\n")
+
+f.close()

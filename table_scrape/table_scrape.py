10694891a53d34929cf6e79aa3cb7d17b7c02bf8
unknown <jlwelsh@gmail.com>
2017-12-14 22:21:34 -0600

    Add csv for player data

--- table_scrape/table_scrape.py
+++ table_scrape/table_scrape.py
@@ -1,14 +1,26 @@
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
+import os
 
 def make_soup(url):
     webpage = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(webpage, "html.parser")
     return soupdata
 
+playerdatasaved = ""
 soup = make_soup("https://www.basketball-reference.com/players/a/")
 
 for record in soup.findAll('tr'):
+    playerdata = ""
     for data in record.findAll('td'):
-        print(data.text)
+        playerdata = playerdata + "," + data.text
+    if len(playerdata) != 0:
+        playerdatasaved = playerdatasaved + "\n" + playerdata[1:]
+
+header = "Player, From, To, Pos, Ht, Wt, Birth Date, College"
+file = open(os.path.expanduser("Basketball.csv"), "wb")
+file.write(bytes(header, encoding = "ascii", errors = "ignore"))
+file.write(bytes(playerdatasaved, encoding="ascii", errors = "ignore"))
+
+print(playerdatasaved)
